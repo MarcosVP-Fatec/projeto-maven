e88aa5ee6088cdeceaf8b2b616a6563af5ebd30a
MarcosVinicioPereira <marcos.vinicio.per@gmail.com>
2020-11-12 02:57:46 +0000

    LogFilter implementado

--- src/main/java/br/gov/sp/fatec/projetomaven/controller/MainController.java
+++ src/main/java/br/gov/sp/fatec/projetomaven/controller/MainController.java
@@ -18,6 +18,7 @@ public class MainController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        
         resp.getWriter().print("<h1>Bem vindo!</h1>"); /*Isso coloca o texto no corpo da resposta*/
     }    
     
