743d2bc9f7647996fcefaa85936f2b42b97c8b12
MarcosVP-Fatec <marcos.pereira47@fatec.sp.gov.br>
2020-11-29 20:20:21 +0000

    Vue - Lançando eventos

--- view/src/App.vue
+++ view/src/App.vue
@@ -5,43 +5,48 @@
       <button @click="incluir">Inserir</button>
     </p>
     <ul>
-      <app-curso-item v-for="(curso, indice) in cursos" @click="excluir(indice)" :key="indice">
-        {{curso.nome}}
+      <app-curso-item v-for="(curso, indice) in cursos" @click="excluir(indice)" :key="indice" :valueDoCursoItem="curso.nome">
+        ( {{ indice + 1}} - {{curso.nome}} )
       </app-curso-item>
+      <!-- Este componente abaixo serve para quando você precisa trocar o que mostrar de acordo com a opção -->
+      <!-- Exemplo dois componentes que recebem o mesmo parâmetro -->
+      <component :is="componenteFixo" valueDoCursoItem="Fixo">( 9999 )</component>
     </ul>
   </div>
 </template>
 
 <script>
-import CursoItem from './components/CursoItem';
-  export default {
-    name: 'app',
-    components: {
-        'app-curso-item': CursoItem
-    },
-    data() {
-      return {
-        novo: '',
-        cursos: [
-          { nome: 'ADS'},
-          { nome: 'Banco de Dados'}
-        ]
-      }
-    },
-    methods: {
-      incluir() {
-        if(this.novo.trim().length == 0) {
-          alert('O nome do curso deve ser preenchido!');
-        } else {
-          this.cursos.push( { nome: this.novo } );
-          this.novo = '';
+    // Importa o componente 
+    import CursoItem from './components/CursoItem';
+    export default {
+        name: 'app',
+        // Exporta o componente para ser usado
+        components: {
+            'app-curso-item': CursoItem // O nome app-curso-item é para ser usado em tags html
+        },
+        data() {
+            return {
+                componenteFixo: 'app-curso-item',
+                novo: '',
+                cursos: [   { nome: 'ADS'           },
+                            { nome: 'Banco de Dados'}
+                        ]
+            }
+        },
+        methods: {
+        incluir() {
+            if(this.novo.trim().length == 0) {
+            alert('O nome do curso deve ser preenchido!');
+            } else {
+            this.cursos.push( { nome: this.novo } );
+            this.novo = '';
+            }
+        },
+        excluir(indice) {
+            if(confirm("Excluir da lista a opção << " + this.cursos[indice].nome + " >> ?")){
+            this.cursos.splice(indice , 1);
+            }
         }
-      },
-      excluir(indice) {
-        if(confirm("Excluir da lista a opção << " + this.cursos[indice].nome + " >> ?")){
-          this.cursos.splice(indice , 1);
         }
-      }
     }
-  }
 </script>
--- view/src/components/CursoItem.vue
+++ view/src/components/CursoItem.vue
@@ -1,11 +1,23 @@
 <template>
-    <li>
-        Nome do curso
+    <li @click="itemClicado">
+        <!-- Nome do curso -->
+        <!-- Exibir a props no lugar do fixo acima -->
+        <!-- A tag <slot/> coloca este conteúdo junto do que foi declarado no app.vue -->
+        {{valueDoCursoItem}} - <slot/>
     </li>
 </template>
 
 <script>
     export default {
-        name: 'CursoItem'
+        name: 'CursoItem',
+        props: {
+            valueDoCursoItem: String
+        },
+        methods: {
+            itemClicado(evento){
+                // Este nome liClick substitui o @click no app.vue, mas usamos o nome padrão click => this.$emit('liClick',evento) 
+                this.$emit('click',evento) 
+            }
+        }
     }
 </script>
